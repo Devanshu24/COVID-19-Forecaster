f668697018c445c07a680fa27550e8d5e6924005
Devanshu24 <devshah.685@live.com>
2021-04-25 15:38:34 +0530

    Add tests fix linting

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+import src.data
+import src.model
+import src.test
+import src.train
+import src.utils  # noqa
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from scipy.signal import savgol_filter
 
+
 def smoothen_data(data):
     return savgol_filter(data, 51, 3)
