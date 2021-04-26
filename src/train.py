37a735fab2ea666a9ea92fe12abb142edbe6cd24
Devanshu24 <devshah.685@live.com>
2021-04-26 23:32:06 +0530

    Improve readability

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ from tqdm import trange
 
 from data import Data
 from model import CoronaVirusPredictor
+from utils import plot_forecast
 
 config = {
     "num_epochs": 120,
@@ -69,13 +70,11 @@ def train_model_with_crossval(
     v = 0
     for train_index, test_index in tscv.split(data):
         v += 1
-        #         print(train_index.shape, test_index.shape)
         X_train, y_train = (data[train_index]), (labels[train_index])
         X_test, y_test = (data[test_index]), (labels[test_index])
 
         train["feats"][v] = X_train.numpy()
         test["feats"][v] = X_test.numpy()
-        #         print(X_train.shape, y_train.shape)
 
         loss_fn = torch.nn.L1Loss(reduction="sum")
         loss_fn2 = torch.nn.MSELoss(reduction="sum")
@@ -89,11 +88,9 @@ def train_model_with_crossval(
         for t in trange(num_epochs):
             model.train()
             model.reset_hidden_state()
-            #             print(f"Inp Shape: {X_train.shape}")
             y_pred = model(X_train.float()).squeeze()
 
             loss = loss_fn2(y_pred.float().flatten(), y_train.float().flatten())
-            # wandb.log({f"train_loss{v}": loss.item()})
             train_hist[t] = loss.item()
 
             optimiser.zero_grad()
@@ -103,15 +100,13 @@ def train_model_with_crossval(
             optimiser.step()
 
         with torch.no_grad():
+            model.eval()
             y_train_pred = model(X_train.float()).squeeze()
 
             train["pred"][v] = y_train_pred.numpy().flatten().tolist()
             train["true"][v] = y_train.numpy().flatten().tolist()
-            model.eval()
             y_test_pred = model(X_test.float()).squeeze()
-            print(y_test_pred.shape)
             test["pred"][v] = y_test_pred.numpy().flatten().tolist()
-            print(len(test["pred"][v]))
             test["true"][v] = y_test.numpy().flatten().tolist()
 
             test_error_mae.append(
@@ -121,32 +116,20 @@ def train_model_with_crossval(
                 loss_fn2(y_test_pred.float().flatten(), y_test.float().flatten()).item()
             )
             r2_scores.append(r2_score(y_test.numpy(), y_test_pred.float().numpy()))
-    #     print(v)
 
     return r2_scores, test_error_mae, test_error_mse, train, test
 
-    # Create a cross val dataset
-
 
 if __name__ == "__main__":
-    # df = pd.DataFrame({"new cases": np.arange(200)})
-    print("I am starting!\n")
     df = pd.read_csv("../data/India_OWID_with_mobility_data.csv")
     dataset = Data(df, 1, 1)
-    # dataset.preprocess()
     dataset.smoothen_df()
-    print(dataset.get_features(config["features"]).count())
-    print(dataset.get_features(config["features"]))
     X, y = dataset.sliding_window(
         config["features"], dataset.previous_days, dataset.forecast_days - 1
     )
-    # X, y = sliding_window(df[df["State"]=="Maharashtra"].drop("State", axis=1), 1)
-    # print(dataset.df)
     print(len(X))
     print(X.shape)
     print(y.shape)
-
-    # exit(0)
     model = CoronaVirusPredictor(1, 1, 1)
     tscv = TimeSeriesSplit()
     r2_scores, test_error_mae, test_error_mse, train, test = train_model_with_crossval(
@@ -156,45 +139,10 @@ if __name__ == "__main__":
         tscv,
     )
     print(r2_scores)
-    # for i in range(1, 6):
-    #     plt.plot(train["loss"][i], label=i)
-    # plt.legend(loc="best")
-    # plt.show()
-    # print(test["true"][1])
     for i in range(1, 6):
-        plt.plot(
-            list(range(0, len(train["pred"][i]))),
-            train["pred"][i],
-            label="Train Predicted",
+        plot_forecast(
+            train["pred"][i], train["true"][i], test["pred"][i], test["true"][i]
         )
-        plt.plot(
-            list(range(0, len(train["true"][i]))), train["true"][i], label="Train True"
-        )
-
-        plt.plot(
-            list(
-                range(
-                    len(train["pred"][i]), len(train["pred"][i]) + len(test["pred"][i])
-                )
-            ),
-            test["pred"][i],
-            label="Test Predicted",
-            linestyle="dotted",
-        )
-        plt.plot(
-            list(
-                range(
-                    len(train["true"][i]), len(train["true"][i]) + len(test["true"][i])
-                )
-            ),
-            test["true"][i],
-            label="Test True",
-            linestyle="dotted",
-        )
-
-        plt.legend()
-
-        plt.show()
 
     exit(0)
     gg, train_hist, test_hist = train_model(model, config, dataset)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,32 @@
+import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
 
 def smoothen_data(data):
     return savgol_filter(data, 51, 3)
+
+
+def plot_forecast(train_pred, train_true, test_pred, test_true):
+    plt.plot(
+        list(range(0, len(train_pred))),
+        train_pred,
+        label="Train Predicted",
+    )
+    plt.plot(list(range(0, len(train_true))), train_true, label="Train True")
+
+    plt.plot(
+        list(range(len(train_pred), len(train_pred) + len(test_pred))),
+        test_pred,
+        label="Test Predicted",
+        linestyle="dotted",
+    )
+    plt.plot(
+        list(range(len(train_true), len(train_true) + len(test_true))),
+        test_true,
+        label="Test True",
+        linestyle="dotted",
+    )
+
+    plt.legend()
+
+    plt.show()
